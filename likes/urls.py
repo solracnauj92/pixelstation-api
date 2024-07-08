15e5a03e4194fbd70f35c296586bcde3802f9f95
Juan Carlos Diaz Lara <juancarlosdiazlara@gmail.com>
2024-07-08 23:24:40 +0000

    Fix unmatched URL routing. Add  Django admin visibility through model registrations

--- likes/urls.py
+++ likes/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from likes import views
 
 urlpatterns = [
-    path('likes/', views.LikeList.as_view(), name='like-list'),
+    path('', views.LikeList.as_view(), name='like-list'),
     path('likes/<int:pk>/', views.LikeDetail.as_view(), name='like-detail'),
 ]
--- pixelstation_api/urls.py
+++ pixelstation_api/urls.py
@@ -17,12 +17,12 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls')),
+    path('', admin.site.urls),
+    path('', include('rest_framework.urls')),
     path('', include('profiles.urls')),
-    path('posts/', include('posts.urls')),
+    path('', include('posts.urls')),
     path('', include('comments.urls')),
     path('', include('likes.urls')),
-
+    
 ]
 
