a4bc33fd26b4d9732a8ff81b97c382ed9dd72f6c
Juan Carlos Diaz Lara <juancarlosdiazlara@gmail.com>
2024-07-08 16:49:51 +0000

    Create 'comments' app with serializers, views, and URLs

--- pixelstation_api/settings.py
+++ pixelstation_api/settings.py
@@ -57,6 +57,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'profiles',
     'posts',
+    'comments',
 
 ]
 
--- pixelstation_api/urls.py
+++ pixelstation_api/urls.py
@@ -21,5 +21,7 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('', include('profiles.urls')),
     path('posts/', include('posts.urls')),
+    path('', include('comments.urls')),
+
 ]
 
