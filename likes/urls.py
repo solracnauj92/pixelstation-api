99ae50151fc43c01254d79f7215aa6850062e6a8
Juan Carlos Diaz Lara <juancarlosdiazlara@gmail.com>
2024-07-08 22:49:22 +0000

    Add Like feature: views, serializers, and URLs and Fix typo error to connect

---
+++ likes/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from likes import views
+
+urlpatterns = [
+    path('likes/', views.LikeList.as_view(), name='like-list'),
+    path('likes/<int:pk>/', views.LikeDetail.as_view(), name='like-detail'),
+]
