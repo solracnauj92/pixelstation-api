d23a8a60aba661bd7f6b33d31e2276e49326ae17
Juan Carlos Diaz Lara <juancarlosdiazlara@gmail.com>
2024-07-09 10:16:10 +0000

    Fix admin configuration issues for Follower model

--- followers/admin.py
+++ followers/admin.py
@@ -3,7 +3,16 @@ from .models import Follower
 
 @admin.register(Follower)
 class FollowerAdmin(admin.ModelAdmin):
-    list_display = ('user', 'followed_user', 'followed_at')
-    search_fields = ('user__username', 'followed_user__username')
-    list_filter = ('followed_at',)
-    ordering = ('-followed_at',)
+    list_display = ('owner', 'followed', 'created_at')
+    list_filter = ('created_at', 'owner', 'followed')
+    search_fields = ('owner__username', 'followed__username')
+    ordering = ('-created_at',)
+
+    def owner(self, obj):
+        return obj.owner.username
+
+    def followed(self, obj):
+        return obj.followed.username
+
+    owner.admin_order_field = 'owner__username'
+    followed.admin_order_field = 'followed__username'
