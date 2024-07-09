818f14d3bcfa70e86bedb3a0ae70999d6750d211
Juan Carlos Diaz Lara <juancarlosdiazlara@gmail.com>
2024-07-09 15:54:14 +0000

    Fixed by applying post  migrations and syncing Django models, urls  with database. Resolved  error.

--- pixelstation_api/urls.py
+++ pixelstation_api/urls.py
@@ -17,8 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', admin.site.urls),
-    path('', include('rest_framework.urls')),
+    path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
     path('', include('profiles.urls')),
     path('', include('posts.urls')),
     path('', include('comments.urls')),
--- posts/serializers.py
+++ posts/serializers.py
@@ -32,9 +32,7 @@ class PostSerializer(serializers.ModelSerializer):
     def get_like_id(self, obj):
         user = self.context['request'].user
         if user.is_authenticated:
-            like = Like.objects.filer(
-                owner=user, post=obj
-            ).first()
+            like = Like.objects.filter(owner=user, post=obj).first()
             return like.id if like else None
         return None
 
