15e30cc39cbd31f6d428918e11112bb1e216bdb2
Juan Carlos Diaz Lara <juancarlosdiazlara@gmail.com>
2024-07-09 10:42:04 +0000

    Added admin.ModelAdmin classes for Forum, Thread, and Post models in admin.py

--- forums/admin.py
+++ forums/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Forum, Thread, Post
 
-# Register your models here.
+
+@admin.register(Forum)
+class ForumAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'created_at')
+    search_fields = ('name', 'description')
+    ordering = ('-created_at',)
+
+@admin.register(Thread)
+class ThreadAdmin(admin.ModelAdmin):
+    list_display = ('title', 'forum', 'creator', 'created_at')
+    list_filter = ('forum', 'creator', 'created_at')
+    search_fields = ('title', 'forum__name', 'creator__username')
+    ordering = ('-created_at',)
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('content', 'thread', 'author', 'created_at')
+    list_filter = ('thread__forum', 'author', 'created_at')
+    search_fields = ('content', 'thread__title', 'author__username')
+    ordering = ('-created_at',)
