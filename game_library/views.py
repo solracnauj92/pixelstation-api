947dc8435c9cbf20340fd913f42e415687836af7
Juan Carlos Diaz Lara <juancarlosdiazlara@gmail.com>
2024-07-09 12:14:49 +0000

    Add GameView and GameCollectionView to views.py

--- game_library/views.py
+++ game_library/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Game, GameCollection
+from .serializers import GameSerializer, GameCollectionSerializer
 
-# Create your views here.
+class GameViewSet(viewsets.ModelViewSet):
+    queryset = Game.objects.all()
+    serializer_class = GameSerializer
+
+class GameCollectionViewSet(viewsets.ModelViewSet):
+    queryset = GameCollection.objects.all()
+    serializer_class = GameCollectionSerializer
+
