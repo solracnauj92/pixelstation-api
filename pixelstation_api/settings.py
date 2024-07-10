04fc693103d40be59057df235bd1dea87f03d93d
Juan Carlos Diaz Lara <juancarlosdiazlara@gmail.com>
2024-07-10 00:15:26 +0000

    Install gunicorn, django-cors-headers, Procfile setup, CORS configuration, JWT settings, and SECRET_KEY management

--- pixelstation_api/settings.py
+++ pixelstation_api/settings.py
@@ -57,6 +57,7 @@ REST_USE_JWT = True
 JWT_AUTH_SECURE = True
 JWT_AUTH_COOKIE = 'my-app-auth'
 JWT_AUTH_REFRESH_COOKIE = 'my-refresh-token'
+JWT_AUTH_SAMESITE = 'None'
 
 REST_AUTH_SERIALIZERS = {
     'USER_DETAILS_SERIALIZER': 'pixelstation_api.serializers.CurrentUserSerializer'
@@ -71,12 +72,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=)j(#v1g$k$)lm&f@$gzd_2=&jutm^yej*o#migi(jgoc9r(l5'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['8000-solracnauj9-pixelstatio-jwocpeoixuv.ws.codeinstitute-ide.net']
+ALLOWED_HOSTS = ['8000-solracnauj9-pixelstatio-jwocpeoixuv.ws.codeinstitute-ide.net'
+'localhost', '<your_app_name>.herokuapp.com']
 
 CSRF_TRUSTED_ORIGINS = [
     'https://8000-solracnauj9-pixelstatio-jwocpeoixuv.ws.codeinstitute-ide.net'
@@ -103,6 +105,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'dj_rest_auth.registration',
+    'corsheaders',
     'profiles',
     'posts',
     'comments',
@@ -118,6 +121,7 @@ SITE_ID = 1
 
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -127,6 +131,16 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'allauth.account.middleware.AccountMiddleware', 
 ]
+
+if 'CLIENT_ORIGIN' in os.environ:
+     CORS_ALLOWED_ORIGINS = [
+         os.environ.get('CLIENT_ORIGIN')
+     ]
+else:
+     CORS_ALLOWED_ORIGIN_REGEXES = [
+         r"^https://.*\.gitpod\.io$",
+     ]
+CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = 'pixelstation_api.urls'
 
