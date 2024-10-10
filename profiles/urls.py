3191511779f84fef8239bfd0cdd15bf3595e75c0
Juan Carlos Diaz Lara <juancarlosdiazlara@gmail.com>
2024-10-10 23:06:26 +0000

    Fix URL routing for profile app to match Frontend

--- profiles/urls.py
+++ profiles/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('profiles/', views.ProfileList.as_view(), name='profile-list'),
-    path('profiles/<int:pk>/', views.ProfileDetail.as_view(), name='profile-detail'),
+    path('<int:pk>/', ProfileDetail.as_view(), name='profile-detail'),
 ]
 
 
