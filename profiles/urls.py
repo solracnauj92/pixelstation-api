22fe5829380126de23883022c085c17aa3db8c75
Juan Carlos Diaz Lara <juancarlosdiazlara@gmail.com>
2024-10-10 23:10:11 +0000

    Revert URL route for profile app to fix Heruko

--- profiles/urls.py
+++ profiles/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('profiles/', views.ProfileList.as_view(), name='profile-list'),
-    path('<int:pk>/', ProfileDetail.as_view(), name='profile-detail'),
+    path('profiles/<int:pk>/', views.ProfileDetail.as_view(), name='profile-detail'),
 ]
 
 
