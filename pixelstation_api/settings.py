3914b6c968a781596e2571e7b69bd6d2444629d4
Juan Carlos Diaz Lara <juancarlosdiazlara@gmail.com>
2024-10-11 08:51:00 +0000

    Replace client origin dev configuration and remove CORS headers code

--- pixelstation_api/settings.py
+++ pixelstation_api/settings.py
@@ -142,11 +142,11 @@ if 'CLIENT_ORIGIN' in os.environ:
         os.environ.get('CLIENT_ORIGIN')
     ]
 if 'CLIENT_ORIGIN_DEV' in os.environ:
-    extracted_url = re.match(r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE).group(0)
-    
-    CORS_ALLOWED_ORIGIN_REGEXES = [r"^https://.*\.codeinstitute-ide\.net$",]
-
+    CORS_ALLOWED_ORIGIN_REGEXES = [
+        os.environ.get('CLIENT_ORIGIN_DEV')
+    ]
 CORS_ALLOW_CREDENTIALS = True
+
 
 CORS_ALLOWED_ORIGINS = [
      'https://pixelstationproject5-17ab85818140.herokuapp.com',
@@ -154,10 +154,6 @@ CORS_ALLOWED_ORIGINS = [
      'http://localhost:3000',
      'http://127.0.0.1:3000',
  ]
-
-CORS_ALLOW_HEADERS = list(default_headers) + [
-    'content-type',
-]
 
 CORS_ALLOW_METHODS = [
     'GET',
