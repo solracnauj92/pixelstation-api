721b0434bba5063e1480be298495bc5248eec208
Juan Carlos Diaz Lara <juancarlosdiazlara@gmail.com>
2024-10-15 19:49:27 +0000

    Modify Profile App views.py  to match original source and fix login issues

--- profiles/views.py
+++ profiles/views.py
@@ -1,68 +1,47 @@
 from django.db.models import Count
-from django.http import Http404
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status, filters, generics
+from rest_framework import generics, filters
 from django_filters.rest_framework import DjangoFilterBackend
+from pixelstation_api.permissions import IsOwnerOrReadOnly
 from .models import Profile
 from .serializers import ProfileSerializer
-from pixelstation_api.permissions import IsOwnerOrReadOnly
 
-# Create your views here.
+
 class ProfileList(generics.ListAPIView):
-    permission_classes = [IsOwnerOrReadOnly]
+    """
+    List all profiles.
+    No create view as profile creation is handled by django signals.
+    """
+    queryset = Profile.objects.annotate(
+        posts_count=Count('owner__post', distinct=True),
+        followers_count=Count('owner__followed', distinct=True),
+        following_count=Count('owner__following', distinct=True)
+    ).order_by('-created_at')
     serializer_class = ProfileSerializer
     filter_backends = [
         filters.OrderingFilter,
-        filters.SearchFilter,
         DjangoFilterBackend,
     ]
-    search_fields = [
-        'owner__username'
-    ]
-
     filterset_fields = [
         'owner__following__followed__profile',
+        'owner__followed__owner__profile',
+    ]
+    ordering_fields = [
+        'posts_count',
+        'followers_count',
+        'following_count',
+        'owner__following__created_at',
+        'owner__followed__created_at',
     ]
 
-    ordering_fields = ['post_count', 'followers_count', 'following_count', 'owner__following__created_at', 'owner__followed__created_at']
 
-    def get_queryset(self):
-        queryset = Profile.objects.annotate(
-            post_count=Count('owner__post', distinct=True),
-            followers_count=Count('owner__followed', distinct=True),
-            following_count=Count('owner__following', distinct=True)
-        ).order_by('-created_at')
-        
-        # Apply filtering and ordering
-        queryset = self.filter_queryset(queryset)
-        return queryset 
-
-class ProfileDetail(APIView):
-    serializer_class = ProfileSerializer
+class ProfileDetail(generics.RetrieveUpdateAPIView):
+    """
+    Retrieve or update a profile if you're the owner.
+    """
     permission_classes = [IsOwnerOrReadOnly]
-
-    def get_object(self, pk):
-        try:
-            profile = Profile.objects.annotate(
-                post_count=Count('owner__post', distinct=True),
-                followers_count=Count('owner__followed', distinct=True),
-                following_count=Count('owner__following', distinct=True)
-            ).get(pk=pk)
-            self.check_object_permissions(self.request, profile)
-            return profile
-        except Profile.DoesNotExist:
-            raise Http404
-
-    def get(self, request, pk):
-        profile = self.get_object(pk)
-        serializer = ProfileSerializer(profile, context={'request': request})
-        return Response(serializer.data)
-
-    def put(self, request, pk):
-        profile = self.get_object(pk)
-        serializer = ProfileSerializer(profile, data=request.data, context={'request': request})
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    queryset = Profile.objects.annotate(
+        posts_count=Count('owner__post', distinct=True),
+        followers_count=Count('owner__followed', distinct=True),
+        following_count=Count('owner__following', distinct=True)
+    ).order_by('-created_at')
+    serializer_class = ProfileSerializer
