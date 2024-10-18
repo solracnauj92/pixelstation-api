b503eac6dd2660276e22d3c29c17a5d2d48929b3
Juan Carlos Diaz Lara <juancarlosdiazlara@gmail.com>
2024-10-18 15:53:09 +0000

    Update URL patterns to include 'game_library/' prefix for game and collection endpoints

--- game_library/urls.py
+++ game_library/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import GameViewSet, GameCollectionViewSet
 
 urlpatterns = [
-    path('games/', GameViewSet.as_view({'get': 'list'}), name='game-list'),
-    path('games/<int:pk>/', GameViewSet.as_view({'get': 'retrieve'}), name='game-detail'),
-    path('collections/', GameCollectionViewSet.as_view({'get': 'list'}), name='collection-list'),
-    path('collections/<int:pk>/', GameCollectionViewSet.as_view({'get': 'retrieve'}), name='collection-detail'),
+    path('game_library/games/', GameViewSet.as_view({'get': 'list'}), name='game-list'),
+    path('game_library/games/<int:pk>/', GameViewSet.as_view({'get': 'retrieve'}), name='game-detail'),
+    path('game_library/collections/', GameCollectionViewSet.as_view({'get': 'list'}), name='collection-list'),
+    path('game_library/collections/<int:pk>/', GameCollectionViewSet.as_view({'get': 'retrieve'}), name='collection-detail'),
 ]
