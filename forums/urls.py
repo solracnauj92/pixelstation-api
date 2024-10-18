c817a141ec0a578a80eed2f19cafd19c9fffe053
Juan Carlos Diaz Lara <juancarlosdiazlara@gmail.com>
2024-10-18 13:01:49 +0000

    Fix URL patterns for clarity and consistency in forum API endpoints

--- forums/urls.py
+++ forums/urls.py
@@ -3,9 +3,9 @@ from forums import views
 
 urlpatterns = [
     path('forums/', views.ForumViewSet.as_view({'get': 'list'}), name='forum-list'),
-    path('forums/<int:pk>/', views.ForumViewSet.as_view({'get': 'retrieve'}), name='forum-detail'),
+    path('forums/<int:forum_id>/', views.ForumViewSet.as_view({'get': 'retrieve'}), name='forum-detail'),
     path('forums/<int:forum_id>/threads/', views.ThreadViewSet.as_view({'get': 'list'}), name='thread-list'),
-    path('forums/<int:forum_id>/threads/<int:pk>/', views.ThreadViewSet.as_view({'get': 'retrieve'}), name='thread-detail'),
+    path('forums/<int:forum_id>/threads/<int:thread_id>/', views.ThreadViewSet.as_view({'get': 'retrieve'}), name='thread-detail'),
     path('forums/<int:forum_id>/threads/<int:thread_id>/posts/', views.PostViewSet.as_view({'get': 'list'}), name='forum-post-list'),
-    path('forums/<int:forum_id>/threads/<int:thread_id>/posts/<int:pk>/', views.PostViewSet.as_view({'get': 'retrieve'}), name='forum-post-detail'),
+    path('forums/<int:forum_id>/threads/<int:thread_id>/posts/<int:post_id>/', views.PostViewSet.as_view({'get': 'retrieve'}), name='forum-post-detail'),
 ]
