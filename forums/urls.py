267e796684c07a14b4a6b97b6ec48ef15ab5e328
Juan Carlos Diaz Lara <juancarlosdiazlara@gmail.com>
2024-10-18 13:25:15 +0000

    Fix route  in the forums app for clarity by simplifying the forum list and detail endpoints

--- forums/urls.py
+++ forums/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from forums import views
+from forums.views import ForumViewSet, ThreadViewSet, PostViewSet
 
 urlpatterns = [
-    path('forums/', views.ForumViewSet.as_view({'get': 'list'}), name='forum-list'),
-    path('forums/<int:forum_id>/', views.ForumViewSet.as_view({'get': 'retrieve'}), name='forum-detail'),
-    path('forums/<int:forum_id>/threads/', views.ThreadViewSet.as_view({'get': 'list'}), name='thread-list'),
-    path('forums/<int:forum_id>/threads/<int:thread_id>/', views.ThreadViewSet.as_view({'get': 'retrieve'}), name='thread-detail'),
-    path('forums/<int:forum_id>/threads/<int:thread_id>/posts/', views.PostViewSet.as_view({'get': 'list'}), name='forum-post-list'),
-    path('forums/<int:forum_id>/threads/<int:thread_id>/posts/<int:post_id>/', views.PostViewSet.as_view({'get': 'retrieve'}), name='forum-post-detail'),
+    path('', ForumViewSet.as_view({'get': 'list'}), name='forum-list'),
+    path('<int:pk>/', ForumViewSet.as_view({'get': 'retrieve'}), name='forum-detail'),
+    path('<int:forum_id>/threads/', ThreadViewSet.as_view({'get': 'list'}), name='thread-list'),
+    path('<int:forum_id>/threads/<int:pk>/', ThreadViewSet.as_view({'get': 'retrieve'}), name='thread-detail'),
+    path('<int:forum_id>/threads/<int:thread_id>/posts/', PostViewSet.as_view({'get': 'list'}), name='forum-post-list'),
+    path('<int:forum_id>/threads/<int:thread_id>/posts/<int:pk>/', PostViewSet.as_view({'get': 'retrieve'}), name='forum-post-detail'),
 ]
