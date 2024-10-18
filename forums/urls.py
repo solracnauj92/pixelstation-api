aefc088b0f3b8816bffd153cc18bffaabd85e6a1
Juan Carlos Diaz Lara <juancarlosdiazlara@gmail.com>
2024-10-18 11:08:44 +0000

    Refactor URL patterns to avoid conflicts between forums and posts apps

--- forums/urls.py
+++ forums/urls.py
@@ -4,8 +4,8 @@ from forums import views
 urlpatterns = [
     path('forums/', views.ForumViewSet.as_view({'get': 'list'}), name='forum-list'),
     path('forums/<int:pk>/', views.ForumViewSet.as_view({'get': 'retrieve'}), name='forum-detail'),
-    path('threads/', views.ThreadViewSet.as_view({'get': 'list'}), name='thread-list'),
-    path('threads/<int:pk>/', views.ThreadViewSet.as_view({'get': 'retrieve'}), name='thread-detail'),
-    path('posts/', views.PostViewSet.as_view({'get': 'list'}), name='post-list'),
-    path('posts/<int:pk>/', views.PostViewSet.as_view({'get': 'retrieve'}), name='post-detail'),
+    path('forums/<int:forum_id>/threads/', views.ThreadViewSet.as_view({'get': 'list'}), name='thread-list'),
+    path('forums/<int:forum_id>/threads/<int:pk>/', views.ThreadViewSet.as_view({'get': 'retrieve'}), name='thread-detail'),
+    path('forums/<int:forum_id>/threads/<int:thread_id>/posts/', views.PostViewSet.as_view({'get': 'list'}), name='forum-post-list'),
+    path('forums/<int:forum_id>/threads/<int:thread_id>/posts/<int:pk>/', views.PostViewSet.as_view({'get': 'retrieve'}), name='forum-post-detail'),
 ]
--- forums/views.py
+++ forums/views.py
@@ -3,11 +3,9 @@ from rest_framework.permissions import IsAuthenticated
 from .models import Forum, Thread, Post
 from .serializers import ForumSerializer, ThreadSerializer, PostSerializer
 
-
 class ForumViewSet(viewsets.ModelViewSet):
     queryset = Forum.objects.all()
     serializer_class = ForumSerializer
-
 
 class ThreadViewSet(viewsets.ModelViewSet):
     queryset = Thread.objects.all()
@@ -17,7 +15,11 @@ class ThreadViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         serializer.save(creator=self.request.user)  
 
-
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user) 
+
