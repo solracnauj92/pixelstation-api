fdb3f718a04381da57a7fdce41d2a78c0b3a3a40
Juan Carlos Diaz Lara <juancarlosdiazlara@gmail.com>
2024-10-24 20:10:37 +0000

    Change messaging app URLs code format to match other apps

--- messaging/urls.py
+++ messaging/urls.py
@@ -1,11 +1,7 @@
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
+from django.urls import path
 from .views import MessageViewSet
 
-
-router = DefaultRouter()
-router.register(r'messages', MessageViewSet)
-
 urlpatterns = [
-    path('', include(router.urls)),
+    path('messages/', MessageViewSet.as_view({'get': 'list', 'post': 'create'}), name='message-list'),
+    path('messages/<int:pk>/', MessageViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'}), name='message-detail'),
 ]
