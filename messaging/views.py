a2bcea0a99d3fb67ded48d3bdcfa904f7eb64c73
Juan Carlos Diaz Lara <juancarlosdiazlara@gmail.com>
2024-10-24 20:36:12 +0000

    Add get_queryset method in MessageViewSet to filter messages by receiver ID

--- messaging/views.py
+++ messaging/views.py
@@ -7,3 +7,10 @@ class MessageViewSet(viewsets.ModelViewSet):
     queryset = Message.objects.all()
     serializer_class = MessageSerializer
     permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        receiver_id = self.request.query_params.get('receiver', None)
+        if receiver_id is not None:
+            queryset = queryset.filter(receiver_id=receiver_id)  
+        return queryset
