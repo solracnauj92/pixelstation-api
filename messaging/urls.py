36fba8cb549fa165ed0b69d94ada2aef59964ebe
Juan Carlos Diaz Lara <juancarlosdiazlara@gmail.com>
2024-10-24 20:27:20 +0000

    Revert back codes as page doesn't load anymore

--- messaging/urls.py
+++ messaging/urls.py
@@ -1,7 +1,11 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from .views import MessageViewSet
 
+
+router = DefaultRouter()
+router.register(r'messages', MessageViewSet)
+
 urlpatterns = [
-    path('messages/', MessageViewSet.as_view({'get': 'list', 'post': 'create'}), name='message-list'),
-    path('messages/<int:pk>/', MessageViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'}), name='message-detail'),
+    path('', include(router.urls)),
 ]
