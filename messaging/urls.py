78e09aef05bf90b7fd4d30efc70af4f09c3a91ce
Juan Carlos Diaz Lara <juancarlosdiazlara@gmail.com>
2024-10-26 17:37:50 +0000

    Revert back urls code as frontend page broke

--- messaging/urls.py
+++ messaging/urls.py
@@ -3,7 +3,8 @@ from rest_framework.routers import DefaultRouter
 from .views import MessageViewSet
 
 router = DefaultRouter()
-router.register(r'messages', MessageViewSet, basename='message')
+router.register(r'api/messages', MessageViewSet, basename='message')
 
 urlpatterns = [
-    path('api/', include(router.urls)), 
+    path('', include(router.urls)),  # Include the router URLs
+]
