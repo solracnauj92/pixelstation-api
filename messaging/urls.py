85f7d17376ab4ad642a92f610e99f30cd600aa2b
Juan Carlos Diaz Lara <juancarlosdiazlara@gmail.com>
2024-10-26 17:51:59 +0000

    Revert back 2 routing urls

--- messaging/urls.py
+++ messaging/urls.py
@@ -2,9 +2,10 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import MessageViewSet
 
+
 router = DefaultRouter()
-router.register(r'api/messages', MessageViewSet, basename='message')
+router.register(r'messages', MessageViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),  # Include the router URLs
+    path('', include(router.urls)),
 ]
