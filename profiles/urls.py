9098933a5fad165c89dbf547c9114b08b784bd82
Juan Carlos Diaz Lara <juancarlosdiazlara@gmail.com>
2024-11-03 20:17:47 +0000

    Fix profiles URL patterns to allow access at /profiles/ and /profiles/<id>/

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
 from profiles import views
 
-
 urlpatterns = [
-    path('profiles/', views.ProfileList.as_view()),
-    path('profiles/<int:pk>/', views.ProfileDetail.as_view()),
+    path('', views.ProfileList.as_view()),  # Now accessible at /profiles/
+    path('<int:pk>/', views.ProfileDetail.as_view()),  # Accessible at /profiles/<id>/
 ]
-
 
