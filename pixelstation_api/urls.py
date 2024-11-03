f7b2e17ae174b4de988a60e522049c5fd136bc0a
Juan Carlos Diaz Lara <juancarlosdiazlara@gmail.com>
2024-11-03 20:06:42 +0000

    Attempt to fix Routing between front and backend of profile

--- pixelstation_api/urls.py
+++ pixelstation_api/urls.py
@@ -1,18 +1,3 @@
-"""pixelstation_api URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 from .views import root_route, logout_route
@@ -25,17 +10,13 @@ urlpatterns = [
     path('dj-rest-auth/', include('dj_rest_auth.urls')),
     path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
     path('accounts/', include('allauth.urls')),
-    path('profiles', include('profiles.urls')),
-    path('posts/', include('posts.urls')),
+    path('profiles/', include('profiles.urls')),
+    path('posts/', include('posts.urls')),       
     path('comments/', include('comments.urls')),
     path('likes/', include('likes.urls')),
-    path('', include('followers.urls')),
+    path('followers/', include('followers.urls')), 
     path('forums/', include('forums.urls')),
     path('replies/', include('replies.urls')),
     path('messaging/', include('messaging.urls')),
-    path('game_library/', include('game_library.urls')), 
-
-
-    
+    path('game_library/', include('game_library.urls')),
 ]
-
