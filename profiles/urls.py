37fb2cdb92483105cd3fe1104473f3aac668b21a
Juan Carlos Diaz Lara <juancarlosdiazlara@gmail.com>
2024-11-03 20:25:29 +0000

    Attempt to fix profile urls routing

--- profiles/urls.py
+++ profiles/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from profiles import views
 
 urlpatterns = [
-    path('', views.ProfileList.as_view()),  # Now accessible at /profiles/
-    path('<int:pk>/', views.ProfileDetail.as_view()),  # Accessible at /profiles/<id>/
+    path('profiles/', views.ProfileList.as_view()),  
+    path('profiles/<int:pk>/', views.ProfileDetail.as_view()),
 ]
 
