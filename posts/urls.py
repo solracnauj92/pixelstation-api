93eeb4eb63554aeb588f35c7f44060b0c22b86ac
Juan Carlos Diaz Lara <juancarlosdiazlara@gmail.com>
2024-11-03 20:51:18 +0000

    Add orginal source code to post views to fix frontend issues

--- posts/urls.py
+++ posts/urls.py
@@ -1,8 +1,7 @@
-# urls.py
 from django.urls import path
 from posts import views
 
 urlpatterns = [
-    path('posts/', views.PostList.as_view(), name='post-list'),  
-    path('posts/<int:pk>/', views.PostDetail.as_view(), name='post-detail')  
+    path('posts/', views.PostList.as_view()),
+    path('posts/<int:pk>/', views.PostDetail.as_view()),
 ]
