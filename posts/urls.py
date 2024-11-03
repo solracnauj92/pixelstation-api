0e4487647cc1bd83ff2c8daff6121bb5cddd973f
Juan Carlos Diaz Lara <juancarlosdiazlara@gmail.com>
2024-11-03 20:39:52 +0000

    Fix profile page error, update routing to post urls

--- posts/urls.py
+++ posts/urls.py
@@ -1,7 +1,8 @@
+# urls.py
 from django.urls import path
 from posts import views
 
 urlpatterns = [
-    path('posts/', views.PostList.as_view()),
-    path('posts/<int:pk>/', views.PostDetail.as_view())
+    path('posts/', views.PostList.as_view(), name='post-list'),  
+    path('posts/<int:pk>/', views.PostDetail.as_view(), name='post-detail')  
 ]
