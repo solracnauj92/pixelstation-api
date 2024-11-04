e102dae8ab8e935673c999b84d0e2298c2b8a6db
Juan Carlos Diaz Lara <juancarlosdiazlara@gmail.com>
2024-11-04 02:12:08 +0000

    Fix views and urls for newsletter

--- newsletter/urls.py
+++ newsletter/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import NewsletterSubscriptionView, subscribe
+from .views import NewsletterSubscriptionView, NewsletterSubscriptionListView, newsletter_root
 
 urlpatterns = [
+    path('', newsletter_root, name='newsletter_root'),  # Redirect or handle root
     path('subscriptions/', NewsletterSubscriptionView.as_view(), name='newsletter_subscribe'),
+    path('subscriptions/list/', NewsletterSubscriptionListView.as_view(), name='newsletter_subscribe_list'),
 ]
--- newsletter/views.py
+++ newsletter/views.py
@@ -1,19 +1,22 @@
-from django.shortcuts import render
-from django.views import View
+# newsletter/views.py
+from rest_framework import generics, permissions
+from django_filters.rest_framework import DjangoFilterBackend
+from .models import NewsletterSubscription
+from .serializers import NewsletterSubscriptionSerializer
+from pixelstation_api.permissions import IsOwnerOrReadOnly
+from django.http import HttpResponseRedirect
+
+def newsletter_root(request):
+    return HttpResponseRedirect('/newsletter/subscriptions/')
+
+class NewsletterSubscriptionView(generics.CreateAPIView):
+    queryset = NewsletterSubscription.objects.all()
+    serializer_class = NewsletterSubscriptionSerializer
+    permission_classes = [permissions.AllowAny]  
 
 
-class NewsletterSubscriptionView(View):
-    def get(self, request):
-        
-        return render(request, 'newsletter/subscribe.html')
-
-    def post(self, request):
-        
-        return render(request, 'newsletter/success.html')
-
-#
-def subscribe(request):
-    if request.method == 'POST':
-       
-        return render(request, 'newsletter/success.html')
-    return render(request, 'newsletter/subscribe.html')
+class NewsletterSubscriptionListView(generics.ListAPIView):
+    queryset = NewsletterSubscription.objects.all()
+    serializer_class = NewsletterSubscriptionSerializer
+    filter_backends = [DjangoFilterBackend]
+   
