2dbc753df14cbf3271fd387121ce6640d6181495
Juan Carlos Diaz Lara <juancarlosdiazlara@gmail.com>
2024-11-04 16:44:18 +0000

    Fix routes of comment for front-end

--- comments/urls.py
+++ comments/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from comments import views
 
 urlpatterns = [
-    path('', views.CommentList.as_view(), name='comment-list'),
-    path('<int:pk>/', views.CommentDetail.as_view(), name='comment-detail'),
+    path('comments/', views.CommentList.as_view()),
+    path('comments/<int:pk>/', views.CommentDetail.as_view())
 ]
