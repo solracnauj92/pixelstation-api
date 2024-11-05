a6a4b2d3a1793aae653550a505145348b78cbc12
Juan Carlos Diaz Lara <juancarlosdiazlara@gmail.com>
2024-11-05 13:44:02 +0000

    Fix , add from .views import HubDetails

--- debatehub/urls.py
+++ debatehub/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import HubList, HubDebateList, DebateDetail
+from .views import HubList, HubDetail, HubDebateList, DebateDetail
 
 urlpatterns = [
     path('hubs/', HubList.as_view(), name='hub-list'),
