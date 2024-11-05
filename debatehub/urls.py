345ac008433e68ca7622aa7ffde989e625686756
Juan Carlos Diaz Lara <juancarlosdiazlara@gmail.com>
2024-11-05 13:40:02 +0000

    Add hub detail urls to fix frontend routing

--- debatehub/urls.py
+++ debatehub/urls.py
@@ -3,6 +3,7 @@ from .views import HubList, HubDebateList, DebateDetail
 
 urlpatterns = [
     path('hubs/', HubList.as_view(), name='hub-list'),
+    path('hubs/<int:hub_id>/', HubDetail.as_view(), name='hub-detail'),  
     path('hubs/<int:hub_id>/debates/', HubDebateList.as_view(), name='hub-debate-list'),
     path('hubs/<int:hub_id>/debates/<int:pk>/', DebateDetail.as_view(), name='debate-detail'),
 ]
